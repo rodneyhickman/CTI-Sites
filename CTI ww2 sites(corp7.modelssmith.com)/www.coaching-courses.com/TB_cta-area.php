f62b823fd5676ea48f87bbef4fd0387be1eb9273
rodney hickman <rodneyhickman@gmail.com>
2015-02-21 16:51:44 -0700

    initial commit

---
+++ CTI ww2 sites(corp7.modelssmith.com)/www.coaching-courses.com/TB_cta-area.php
@@ -0,0 +1,70 @@
+
+<div id="cta-area">
+<dl class="cta" id="cta1">
+
+<dt><a href="../leadership/index.html"><img src="/res/img/cta_leadership.gif" width="195" height="42" alt="Leadership: Why Now?" /></a></dt>
+
+<dd><a href="../leadership/index.html">There is no more important time to have a powerful, positive and lasting impact within your organization, profession, family or community<span class="arrowlink">.</span></a></dd>
+</dl>
+
+<dl class="cta" id="cta2">
+
+<dt><a href="http://www.coactivenetwork.com/"><img src="/res/img/cta_join-our-network.gif" width="195" height="42" alt="Join Our Network" /></a></dt>
+
+<dd><a href="http://www.coactivenetwork.com/">The universal gathering place for <abbr title="Coaches Training Institute">CTI</abbr> graduates to assemble in one central community
+
+<span class="arrowlink">.</span>
+
+</a></dd>
+
+</dl>
+
+<dl class="cta" id="cta3">
+
+<dt><a href="../contact/schedule-a-call/index.html"><img src="/res/img/cta_schedule-a-call.gif" width="195" height="42" alt="Schedule a Call" /></a></dt>
+
+<dd><a href="../contact/schedule-a-call/index.html">Do you want to hear more about Co-active Coaching? Please feel free to contact <abbr title="Coaches Training Institute">CTI</abbr> at +44(0)845 299 8199 or click here to register
+
+<span class="arrowlink">.</span>
+
+</a></dd>
+
+</dl>
+
+
+<dl class="cta" id="cta4">
+
+<dt><a href="/coach-training/assessments-and-tools/"><img src="http://www.thecoaches.com/res/img/cta_assessment.gif" width="195" height="42" alt="Assessments and Tools" /></a></dt>
+
+<dd><a href="/coach-training/assessments-and-tools/" style="color:#699bbc;"><b><span style="font-size:1.2em">The Leadership Circle Profile</span><br />The breakthrough 360 degree profile that gives you the whole picture</b></a>.<br />
+
+</dd>
+</dl>
+
+<!-- #startcta4 -->
+<!-- newsletter signup temporarily commented out
+<dl class="cta" id="cta4">
+          <dt><img src="res/img/cta_newsletter-sign-up.gif" width="195" height="42" alt="Newsletter Sign-Up" /></dt>
+
+          <dd>
+            <script language="Javascript" src="http://builder.campaigner.com/app/campaigner/calendar/calendar.js" type="text/javascript">
+</script><script language="JavaScript" type="text/javascript">
+//<![CDATA[
+function popUp(url)
+{
+ appWindows = window.open(url, "Member", "toolbar=0,location=0,status=0,menubar=0,scrollbars=1,resizable=0,width=400,height=250");
+}
+//]]>
+</script> 
+
+
+            <form id="subscribe" name="optin" target="Member" method="post" action="http://builder.campaigner.com/app/campaigner/services/optinlist/processoptinrequest.jsp?oilb=90754287&amp;builderType=free">
+
+              <input type="hidden" name="OILB_MAIL_PREF" value="multi-part" /><input id="newsletter" name="newsletter" type="text" /><input value="Submit" onclick="javascript:popUp('');document.optin.submit();" id="submit" name="submit" type="image" src="res/img/arrow_submit.jpg" alt="&gt;" /> <label id="label_newsletter" for="newsletter">Enter your email address above</label> <input type=
+              "HIDDEN" name="commitcontact" value="true" /><input type="HIDDEN" name="builderType" value="free" />
+            </form>
+          </dd>
+        </dl>
+-->          
+<!-- #end -->
+</div>
