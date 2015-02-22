76de60721c5c2d829c0f6cb2f15b25865ef3bcd0
rodney hickman <rodneyhickman@gmail.com>
2015-02-21 17:30:50 -0700

    initial commit

---
+++ CTI www.thecoaches.com (comp9.modelsmith.com)/www.coactivesummit.com/wp-content/themes/flare-child-theme/functions.php
@@ -0,0 +1,27 @@
+<?php
+/* Prevent direct script access */
+if ( !empty( $_SERVER[ 'SCRIPT_FILENAME' ] ) && 'functions.php' == basename( $_SERVER['SCRIPT_FILENAME'] ) ) {
+	die ( 'No direct script access allowed' );
+}
+
+
+
+
+
+/**
+* Flare Child Theme Setup
+* 
+* Always use child theme if you want to make some custom modifications. 
+* This way theme updates will be a lot easier.
+*/
+function btp_flarechild_setup() {
+
+	
+	
+	
+}
+add_action( 'after_setup_theme', 'btp_flarechild_setup' );
+
+add_filter('g1_breadcrumbs', '__return_empty_array', 999);
+?>
+
---
+++ CTI www.thecoaches.com (comp9.modelsmith.com)/www.coactivesummit.com/wp-content/themes/flare/precontent.php
@@ -0,0 +1,14 @@
+<div id="precontent" class="<?php echo btp_content_get_class(); ?>">
+	<div id="precontent-inner">
+		<?php btp_precontent(); ?>		
+		<?php get_template_part( 'content_header' ); ?>
+	</div>
+	<div class="background">
+		<div class="shadow"></div>
+		<div class="pattern"></div>
+		<div class="flare">
+			<div></div>
+			<div></div>
+		</div>
+	</div>
+</div>
