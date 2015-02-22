2857cbab2287a56eac80230a9c2e4cbb25568812
rodney hickman <rodneyhickman@gmail.com>
2015-02-21 17:51:25 -0700

    initial commit

---
+++ CTI www.thecoaches.com (comp9.modelsmith.com)/www.thecoaches.com/_base_symfony_project/lib/model/GroupPeer.php
@@ -0,0 +1,11 @@
+<?php
+
+class GroupPeer extends BaseGroupPeer
+{
+
+  public static function currentGroup(){
+    // get most recent group
+    return null;
+  }
+
+}
---
+++ CTI www.thecoaches.com (comp9.modelsmith.com)/www.thecoaches.com/_base_symfony_project/lib/model/HomeworkPeer.php
@@ -0,0 +1,19 @@
+<?php
+
+class HomeworkPeer extends BaseHomeworkPeer
+{
+  
+  public static function retrieveCurrentByProfile( $profile_id ){
+    // create new record if necessary
+    return null;
+  } 
+
+  public static function csvReportByGroup( $group_id ){
+    return '';
+  }
+
+  public static function reportByGroup( $group_id ){
+    return '';
+  }
+
+}
